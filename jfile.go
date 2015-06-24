d9213e11ca293e9e2705217ed372f53467d079ab
Alexander F Rødseth <rodseth@gmail.com>
2015-06-24 23:14:57 +0200

    New function for retrieving the filename

--- jfile.go
+++ jfile.go
@@ -33,6 +33,11 @@ func NewFile(filename string) (*JFile, error) {
 	return &JFile{filename, js, rw, true}, nil
 }
 
+// GetFilename returns the current filename
+func (jf *JFile) GetFilename() string {
+	return jf.filename
+}
+
 // SetPretty can be used for setting the "pretty" flag to true, for indenting
 // all JSON output. Set to true by default.
 func (jf *JFile) SetPretty(pretty bool) {
